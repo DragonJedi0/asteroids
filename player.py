e8c853d46e46b373d1b71ee396f929f895db8a06
Samuel Ybarra <sybarra2076@gmail.com>
2025-05-19 14:24:15 -0500

    Limiting bullet spawn

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(pos_x, pos_y, PLAYER_RADIUS)
         self.rotation = 0
         self.shot_group = shot_group
+        self.__rate_limit = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -21,6 +22,7 @@ class Player(CircleShape):
         return [a, b, c]
     
     def update(self, dt):
+        self.__rate_limit -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
@@ -36,7 +38,9 @@ class Player(CircleShape):
             self.move(-dt)
         
         if keys[pygame.K_SPACE]:
-            self.shoot(self.shot_group)
+            if self.__rate_limit <= 0:
+                self.shoot(self.shot_group)
+            self.__rate_limit = PLAYER_SHOOT_COOLDOWN
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
