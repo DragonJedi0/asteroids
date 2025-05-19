f862fd12fb8a5788993c8d5ff164f1b9bc1b0f6f
Samuel Ybarra <sybarra2076@gmail.com>
2025-05-19 09:27:21 -0500

    Limiting Framerate

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while pygame.get_init:
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+
+        # Limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
